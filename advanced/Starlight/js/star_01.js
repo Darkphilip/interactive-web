05e439dc25231078e84317bd8caa9b751d149ef2
Darkphilip <yoonlucky0@gmail.com>
2022-02-06 10:22:10 +0900

    starlight web coding initial commit

---
+++ advanced/Starlight/js/star_01.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  let $starBg = document.querySelector(".starBg");
+  let $title = document.querySelector(".title");
+
+  window.addEventListener("scroll", function(event) {
+    let scroll = this.scrollY;
+    $starBg.style.transform = "translateY(" + -scroll/3 +"px)";
+    $title.style.transform = "translateY(" + scroll/1.7 +"px)";
+  });
+
+  setTimeout(() => {
+    window.scrollTo({
+      top: document.querySelector('.bottom').offsetTop,
+      behavior: 'smooth'
+    });
+  }, 2000)
+}
