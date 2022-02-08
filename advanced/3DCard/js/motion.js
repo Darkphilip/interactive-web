0572f4b5f465e61360a9048aa90006ee97d4adb4
Darkphilip <yoonlucky0@gmail.com>
2022-02-08 11:21:29 +0900

    3D Card mobile code commit

--- advanced/3DCard/js/motion.js
+++ advanced/3DCard/js/motion.js
@@ -1,19 +1,59 @@
-var wrap;
-var x = 0, y = 0;
-var mx = 0, my = 0;
+let wrap;
+let x = 0, y = 0;
+let mx = 0, my = 0;
+let isMobile = false;
+let isIos = false;
 
 window.onload = function () {
     wrap = document.querySelector(".contentWrap");
+    isMobile = mobileChk();
+    isIos = iosChk();
 
-    window.addEventListener("mousemove", function (e) {
+    let button = document.querySelectorAll("button")[0];
 
-        // console.log(e.clientY-window.innerHeight / 2)
-        x = (e.clientX - window.innerWidth / 2);
-        y = (e.clientY - window.innerHeight / 2);
-        //마우스 위치값을 화면의 정가운데가 0,0이 되도록 맞춤
-        // console.log(x, y);
+    button.addEventListener("click", function () {
+        button.classList.add("dimd");
+        wrap.classList.add("active");
+
+        if (isMobile) {
+            //모바일이면 실행
+            if(isIos){
+                //ios일때만 실행
+                DeviceOrientationEvent.requestPermission().then(function () {
+                    // console.log('DeviceOrientationEvent, DeviceMotionEvent enabled');
+                    mobileOrientationChk();
+                }).catch( console.error );
+                
+            }else{
+                mobileOrientationChk();
+            }
+
+            function mobileOrientationChk(){
+                window.addEventListener("deviceorientation", function (event) {
+                    //디바이스가 움직임 감지될때 실행
+                    x = event.gamma;
+                    y = event.beta;
+                });
+                loopMobile();
+            }
+
+        } else {
+            //pc면 실행
+            window.addEventListener("mousemove", function (e) {
+                x = (e.clientX - window.innerWidth / 2);
+                y = (e.clientY - window.innerHeight / 2);
+                //마우스 위치값을 화면의 정가운데가 0,0이 되도록 맞춤
+            });
+            loop();
+        }
     });
-    loop();
+}
+
+function loopMobile() {
+    mx += (x - mx) * .1;
+    my += (y - my) * .1;
+    wrap.style.transform = "translate3d(-50%, -50%, 0) rotateX(" + (my - 50) + "deg) rotateY(" + (mx) + "deg)";
+    window.requestAnimationFrame(loopMobile);
 }
 
 function loop() {
@@ -22,9 +62,29 @@ function loop() {
     //가속도 설정. 뒤의 값을 변경하면 가속도 값 변경
 
     wrap.style.transform = "translate3d(-50%, -50%, 0) rotateX(" + (my / 10) + "deg) rotateY(" + (-mx / 10) + "deg)";
-    // //마우스 위치에 따른 대상의 움직임 위치 셋팅
+    //마우스 위치에 따른 대상의 움직임 위치 셋팅
 
     window.requestAnimationFrame(loop);
     //반복 실행
 }
 
+
+function mobileChk() {
+    let mobileKeyWords = new Array('Android', 'iPhone', 'iPad', 'BlackBerry', 'Windows CE', 'SAMSUNG', 'LG', 'MOT', 'SonyEricsson');
+    for (let info in mobileKeyWords) {
+        if (navigator.userAgent.match(mobileKeyWords[info]) != null) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function iosChk(){
+    var mobileKeyWords = new Array('iPhone', 'iPad');
+    for (let info in mobileKeyWords) {
+        if (navigator.userAgent.match(mobileKeyWords[info]) != null) {
+            return true;
+        }
+    }
+    return false;
+}
